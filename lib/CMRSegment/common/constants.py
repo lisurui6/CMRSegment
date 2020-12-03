4a40e014308844f6e79c6aab24eae99b376172a2
Surui Li <lisurui6@gmail.com>
2020-12-03 15:41:54 +0000

    fix lib path

--- lib/CMRSegment/common/constants.py
+++ lib/CMRSegment/common/constants.py
@@ -2,5 +2,5 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.parent.parent.parent
 MODEL_DIR = ROOT_DIR.joinpath("models")
 DATA_DIR = ROOT_DIR.joinpath("data")
-LIB_DIR = ROOT_DIR.joinpath("lib")
+LIB_DIR = Path(__file__).parent.parent.parent
 RESOURCE_DIR = LIB_DIR.joinpath("CMRSegment", "resource")
