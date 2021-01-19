75d6c2d60cd91ed49c825041def0f0815c1651f3
lisurui <surui@scape.io>
2021-01-19 23:11:59 +0800

    add affine uda exp

--- experiments/uda_def_seg_bidir_affine/train.py
+++ experiments/uda_def_seg_bidir_affine/train.py
@@ -78,7 +78,6 @@ def main():
         n_slices=get_conf(train_conf, group="network", key="n_slices"), is_3d=True, seed=config.seed,
         augmentation_config=augmentation_config, output_dir=config.experiment_dir,
         template_path=Path(get_conf(train_conf, group="network", key="template_path")),
-        template_image_path=Path(get_conf(train_conf, group="network", key="template_image_path")),
     )
     for train in training_sets:
         train.export(config.experiment_dir.joinpath("training_set_{}.csv".format(train.name)))
