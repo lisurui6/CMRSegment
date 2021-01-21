3a623499c5f91c98fb702b296adcaf9a1230ecef
lisurui <surui@scape.io>
2021-01-21 12:58:00 +0800

    update affine sep

--- experiments/def_seg_bidir_affine_no_seg/network.py
+++ experiments/def_seg_bidir_affine_no_seg/network.py
@@ -38,11 +38,17 @@ class AffineLocalNet(torch.nn.Module):
         super().__init__()
         activation = torch.nn.ReLU
         self.conv1 = conv_block_2_3d(in_dim, 16, activation, batch_norm, group_norm)
-        self.maxpoo1 = nn.MaxPool3d(kernel_size=4, stride=4, padding=0)
-        self.conv2 = conv_block_2_3d(16, 8, activation, batch_norm, group_norm)
-        self.maxpoo2 = nn.MaxPool3d(kernel_size=4, stride=4, padding=0)
-        self.conv3 = torch.nn.Sequential(
-            torch.nn.Conv3d(8, 1, kernel_size=1),
+        self.maxpoo1 = nn.MaxPool3d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = conv_block_2_3d(16, 32, activation, batch_norm, group_norm)
+        self.maxpoo2 = nn.MaxPool3d(kernel_size=2, stride=2, padding=0)
+        self.conv3 = conv_block_2_3d(32, 64, activation, batch_norm, group_norm)
+        self.maxpoo3 = nn.MaxPool3d(kernel_size=2, stride=2, padding=0)
+        self.conv4 = conv_block_2_3d(64, 32, activation, batch_norm, group_norm)
+        self.maxpoo4 = nn.MaxPool3d(kernel_size=2, stride=2, padding=0)
+        self.conv5 = conv_block_2_3d(32, 16, activation, batch_norm, group_norm)
+        self.maxpoo5 = nn.MaxPool3d(kernel_size=2, stride=2, padding=0)
+        self.conv6 = torch.nn.Sequential(
+            torch.nn.Conv3d(16, 1, kernel_size=1),
             nn.BatchNorm3d(1),
             activation(),
         )
@@ -63,6 +69,13 @@ class AffineLocalNet(torch.nn.Module):
         out = self.conv2(out)
         out = self.maxpoo2(out)
         out = self.conv3(out)
+        out = self.maxpoo3(out)
+        out = self.conv4(out)
+        out = self.maxpoo4(out)
+        out = self.conv5(out)
+        out = self.maxpoo5(out)
+
+        out = self.conv6(out)
         out = out.view(out.shape[0], -1)
         theta = self.regress(out)
         theta = theta.view(theta.shape[0], 3, 4)
@@ -74,13 +87,7 @@ class DefSegNet(torch.nn.Module):
                  int_downsize=1, bidir=False, batch_norm=True, group_norm=0):
         assert not (batch_norm and group_norm)
         super().__init__()
-        self.seg_unet = UNet(
-            in_channels=in_channels,
-            n_classes=n_classes,
-            n_filters=n_filters,
-            batch_norm=batch_norm,
-            group_norm=group_norm,
-        )
+
         enc_nf = [16, 32, 32, 32]
         dec_nf = [32, 32, 32, 32, 32, 16, 16]
         self.vxm_dense = VxmDense(
